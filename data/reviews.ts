39ec1c0ae21a6d903689b8604a3a997209347fb8
SamuelVapps1 <samovazan@gmail.com>
2026-06-05 02:37:54 +0200

    Fix review wording

--- data/reviews.ts
+++ data/reviews.ts
@@ -18,7 +18,7 @@ export const REVIEWS: Review[] = [
   {
     id: 'r2',
     quote:
-      'Veľmi príjemný personál. Maximálne sa venujú Vášmu miláčikovi. Psík od nich vždy odchádza nádherne ostrihaný. Majú široký výber cez jedlo, pamlsky, obojky, oblečenie a hračky a to aj pre mačičky, hlodavce a vtáčiky. VRELO odporúča',
+      'Veľmi príjemný personál. Maximálne sa venujú Vášmu miláčikovi. Psík od nich vždy odchádza nádherne ostrihaný. Majú široký výber cez jedlo, pamlsky, obojky, oblečenie a hračky a to aj pre mačičky, hlodavce a vtáčiky. VRELO odporúčam',
     name: 'Ivana K.',
     initials: 'P',
     source: 'Google recenzia',
@@ -26,7 +26,7 @@ export const REVIEWS: Review[] = [
   {
     id: 'r3',
     quote:
-      'Malá predajňa úplne narávaná tovarom pre domácich miláčikov a salón k tomu. Za veľmi slušné peniaze všetko možné, ak nemajú tak ochotne objednajú za ceny ktoré sú jednoducho super.',
+      'Malá predajňa úplne narvaná tovarom pre domácich miláčikov a salón k tomu. Za veľmi slušné peniaze všetko možné, ak nemajú tak ochotne objednajú za ceny ktoré sú jednoducho super.',
     name: 'Branislav Z.',
     initials: 'Z',
     source: 'Google recenzia',
